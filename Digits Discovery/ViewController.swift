d51fa10e7f499403dc8f06f6e5412b671cbdbdf6
Birnroman <99803046+Birnroman@users.noreply.github.com>
2023-08-21 14:01:33 +0300

    Initial Commit

---
+++ Digits Discovery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Digits Discovery
+//
+//  Created by Rum Gersy on 21.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
